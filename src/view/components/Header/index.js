7aa26fa10b8520f5e572f51523bf6eac47d16bd8
ahn <ansb1029@gmail.com>
2021-03-01 00:04:56 +0900

    header 추가

--- src/view/components/Header/index.js
+++ src/view/components/Header/index.js
@@ -6,19 +6,55 @@ const Header = () => {
 
     return (
         <Container>
-            <NavItem>
-                <NavLink to={'/'}>home</NavLink>
-            </NavItem>
+            <Logo>Logo</Logo>
+            <Nav>
+            <NavItem>home</NavItem>
+            <NavItem>about</NavItem>
+            <NavItem>works</NavItem>
+            <NavItem>skills</NavItem>
+            <NavItem>contact</NavItem>
+            </Nav>
         </Container>
     )
 }
 
 const Container = styled.div`
     display:flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  background: #fff;
+  box-shadow: 1px 1px 10px rgba(0,0,0,0.15);
 `;
 
+const Logo = styled.div`
+    font-size: 20px;
+  font-weight: bold;
+  color: #333;
+`
+
+const Nav = styled.div`
+    
+    display: flex;
+    
+`
+
 const NavItem = styled.div`
-    padding: 20px 30px;
+
+  display: flex;
+  align-items: center;
+  padding: 0px 20px;
+  height: 65px;
+  color: #333;
+  text-transform: capitalize;
+  &:hover{
+    color: #18f;
+  }
 `;
 
 const NavLink = styled(Link)`
